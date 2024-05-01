7dc5db1c9b26f76a3715e231943be6850b4b8789
G1YWty13 <rstoyo3.watanabe.yuki@techcamp-training.com>
2024-05-01 18:05:51 +0900

    ユーザー認証機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  # 非ログイン時にログイン画面に遷移
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+  # device用のストロングパラメーター設定。 nameを許可している。
+
 end
+
