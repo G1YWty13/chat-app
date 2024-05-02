6ec0a287598184e1d2a84137aa2efd138516727a
G1YWty13 <rstoyo3.watanabe.yuki@techcamp-training.com>
2024-05-02 13:48:50 +0900

    room_usersテーブルの作成

--- db/migrate/20240501080753_devise_create_users.rb
+++ db/migrate/20240501080753_devise_create_users.rb
@@ -2,6 +2,7 @@
 
 class DeviseCreateUsers < ActiveRecord::Migration[7.0]
   def change
+    
     create_table :users do |t|
       ## Database authenticatable
       t.string :name,               null: false
